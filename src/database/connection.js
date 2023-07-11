e25333a642630daa72cd336add13ffa22450d329
alissonromaosantos <eualissonromaosantos@gmail.com>
2023-07-11 19:30:54 -0300

    Fix arquivo knexfile.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,11 @@
 require("dotenv").config({  
-  path: process.env.NODE_ENV.trim() == "test" ? ".env.test" : ".env"
+  path: process.env.NODE_ENV == "test" ? ".env.test" : ".env"
 })
 
 const { knex } = require("knex");
 const knexfile = require("../../knexfile");
 
-const environment =  process.env.NODE_ENV.trim() ?? "development"
+const environment =  process.env.NODE_ENV ?? "development"
 const configDb = knexfile[environment]
 
 module.exports = knex(configDb);
